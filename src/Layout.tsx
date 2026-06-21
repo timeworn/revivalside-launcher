ca8e07aabfa71a6e81725e3fb0e554867ed9b7c5
timeworn <contact@timeworn.net>
2026-06-21 00:21:32 -0700

    feat: page transitions

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,16 +1,48 @@
 import { Sidebar, type SidebarNavItem } from "@/components/Sidebar";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { Outlet } from "react-router-dom";
-import type { FC } from "react";
+import { useLocation, useOutlet } from "react-router-dom";
+import { type FC } from "react";
+import { cn } from "@/lib/utils";
+import { AnimatePresence, motion } from "motion/react";
 
 export const Layout: FC<{ items: SidebarNavItem[] }> = ({ items }) => {
+  const outlet = useOutlet();
+  const location = useLocation();
+  const isHome = location.pathname === "/";
+
   return (
     <TooltipProvider>
-      <div className="w-screen h-screen flex bg-[url(/bg.png)] bg-cover overflow-hidden">
+      <div className="w-screen h-screen flex overflow-hidden">
+        <div className="absolute -z-50 h-full w-full">
+          <img src="/bg.png" className="w-full h-full object-cover" />
+        </div>
+        <div className="absolute -z-40 h-full w-full bg-background/50" />
         <Sidebar items={items} />
-        <main className="flex-1">
-          <Outlet />
-        </main>
+        <div
+          className={cn(
+            "flex-1 transition-[backdrop-filter] duration-300",
+            !isHome && "w-full h-full backdrop-blur-3xl",
+          )}
+        >
+          <AnimatePresence mode="wait" initial={false}>
+            <motion.div
+              key={location.pathname}
+              initial={{ opacity: 0, y: 40 }}
+              animate={{
+                opacity: 1,
+                y: 0,
+                transition: { duration: 0.25, delay: 0.2, ease: [0.25, 0.1, 0.25, 1] },
+              }}
+              exit={{
+                opacity: 0,
+                transition: { duration: 0.15, ease: [0.25, 0.1, 0.25, 1] },
+              }}
+              className={cn("w-full h-full", !isHome && "p-14")}
+            >
+              {outlet}
+            </motion.div>
+          </AnimatePresence>
+        </div>
       </div>
     </TooltipProvider>
   );
